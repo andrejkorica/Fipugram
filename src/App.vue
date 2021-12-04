49b9862e47b49902ce1432eb761a15581d19fe8e
Andrej Korica <koricaandrej3@gmail.com>
2021-12-04 18:23:30 +0100

    Do epizode 3

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <!-- Image and text -->
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/12539-middle.png" height="30" alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">Registracija</router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Pretraga"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-7">
+        <instagram-card/>
+        <instagram-card/>
+        <instagram-card/>
+        
+      </div>
+      <div class="col-3">... ovdje ide desni stupac!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import InstagramCard from "@/components/InstagramCard.vue";
+
+export default {
+  name: "home",
+  components: {
+    InstagramCard,
+  },
+};
+</script>
+
+
